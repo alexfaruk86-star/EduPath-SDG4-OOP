8047c7515f9570c3d1d69bfd715beca3750ee86e
Faruk Omar <alexfaruk86@gmail.com>
2026-08-18 11:53:35 +0000

    Add Question class for quiz assessment

---
+++ src/model/Question.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Question {
+
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String correctAnswer;
+
+
+    public Question(String questionText,
+                    String optionA,
+                    String optionB,
+                    String optionC,
+                    String correctAnswer) {
+
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.correctAnswer = correctAnswer;
+
+    }
+
+
+    public String getQuestionText() {
+
+        return questionText;
+
+    }
+
+
+    public String getOptionA() {
+
+        return optionA;
+
+    }
+
+
+    public String getOptionB() {
+
+        return optionB;
+
+    }
+
+
+    public String getOptionC() {
+
+        return optionC;
+
+    }
+
+
+    public boolean checkAnswer(String answer) {
+
+        return correctAnswer.equalsIgnoreCase(answer);
+
+    }
+
+
+    public void displayQuestion() {
+
+        System.out.println(questionText);
+        System.out.println("A. " + optionA);
+        System.out.println("B. " + optionB);
+        System.out.println("C. " + optionC);
+
+    }
+
+}
