a87434e6b780fa6e754b653bcacf6159a79f360b
Faruk Omar <alexfaruk86@gmail.com>
2026-08-18 11:56:58 +0000

    Add Progress class for student learning tracking

---
+++ src/model/Progress.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Progress {
+
+    private Student student;
+    private Course course;
+    private int completedPercentage;
+    private int quizScore;
+
+
+    public Progress(Student student, Course course) {
+
+        this.student = student;
+        this.course = course;
+        this.completedPercentage = 0;
+        this.quizScore = 0;
+
+    }
+
+
+    public void updateProgress(int percentage) {
+
+        this.completedPercentage = percentage;
+
+    }
+
+
+    public void updateQuizScore(int score) {
+
+        this.quizScore = score;
+
+    }
+
+
+    public void displayProgress() {
+
+        System.out.println("Student: " + student.getName());
+        System.out.println("Course: " + course.getCourseName());
+        System.out.println("Completion: " 
+                + completedPercentage + "%");
+        System.out.println("Quiz Score: " 
+                + quizScore);
+
+    }
+
+}
